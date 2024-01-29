386f8de69174a02823be71f39763510c3e7008c8
Xoui <136085758+Xoui@users.noreply.github.com>
2024-01-29 18:05:33 +0000

    BARE SERVER v3 BABY

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import createBareServer from '@tomphttp/bare-server-node';
+import { createBareServer } from '@tomphttp/bare-server-node';
 import { fileURLToPath } from "url";
 import { createServer as createHttpsServer } from "node:https";
 import { createServer as createHttpServer } from "node:http";
